241356bfb8be6823d548a9b16f7ca1d8f7c87b07
Guido Gloor <haslo@haslo.ch>
2022-05-22 13:03:26 +0200

    MUI added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 
 import {useState, useEffect} from "react";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
 
   const [newsPosts, setNewsPosts] = useState(null);
