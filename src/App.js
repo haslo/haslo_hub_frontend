2ba4b556e786a60b592c88627f87afb00dc61097
Guido Gloor <haslo@haslo.ch>
2022-05-22 12:21:58 +0200

    calling GraphQL for content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState, useEffect} from "react";
+
 function App() {
+
+  const [newsPosts, setNewsPosts] = useState(null);
+  const [apiErrors, setApiErrors] = useState(null);
+  const [query] = useState(`
+  {
+    newsPostCollection {
+      items {
+        title,
+        description,
+        originalUrl,
+        thumbnailImage {
+          contentType
+          url
+        },
+        type {
+          title,
+          channelUrl,
+          icon {
+            contentType
+            url
+          }
+        }
+      }
+    }
+  }
+  `)
+
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/wehngbocf979/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer ck9CqB4ydwUdbKGF6quZzIqVS0JvmlMhWQJnFSnODrA",
+        },
+        body: JSON.stringify({query}),
+      })
+      .then((response) => response.json())
+      .then(({data, errors}) => {
+        if (errors) {
+          console.error(errors);
+          setApiErrors(errors.toString());
+        }
+        setNewsPosts(data.newsPostCollection.items);
+      });
+  }, [query]);
+
+  if (!newsPosts) {
+    return (
+      <div className="App">
+        Loading...<br/>
+        {apiErrors}
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {newsPosts.map(newsPost => (
+        <div className="news-post">
+          {newsPost.title}
+        </div>
+      ))}
     </div>
   );
 }
