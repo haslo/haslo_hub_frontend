6012f651d3adada9b32295ad4814679021400316
Guido Gloor <haslo@haslo.ch>
2022-08-19 20:03:36 +0200

    Removed fallback link

--- src/SoundCloudIFrame.js
+++ src/SoundCloudIFrame.js
@@ -4,11 +4,7 @@ function SoundCloudIFrame() {
   const iframeHtml = '<iframe ' +
     'width="100%" height="200" scrolling="no" frameborder="no" allow="autoplay" ' +
     'src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1327122892%3Fsecret_token%3Ds-6JcvszCw0c7&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">' +
-    '</iframe>' +
-    '<div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">' +
-    '<a href="https://soundcloud.com/glodjib" title="haslo" target="_blank" style="color: #cccccc; text-decoration: none;">haslo</a> · ' +
-    '<a href="https://soundcloud.com/glodjib/rat-poison/s-6JcvszCw0c7" title="Rat Poison" target="_blank" style="color: #cccccc; text-decoration: none;">Rat Poison</a>' +
-    '</div>'
+    '</iframe>'
   return (
     <Container fixed style={{marginTop: '100px'}}>
       <div dangerouslySetInnerHTML={{__html: iframeHtml}}/>
