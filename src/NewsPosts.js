e5cf70024f5a83b4fa891dc14faa17638501ec65
Guido Gloor <haslo@haslo.ch>
2022-09-01 10:20:18 +0200

    Endless scrolling with Waypoint

--- src/NewsPosts.js
+++ src/NewsPosts.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from "react";
+import {Waypoint} from "react-waypoint";
 
 import {Container, Grid} from "@mui/material";
 import Typography from "@mui/material/Typography";
@@ -8,6 +9,7 @@ import NewsPostCard from "./NewsPostCard";
 function NewsPosts({searchQuery, isFiltering}) {
 
   const [newsPosts, setNewsPosts] = useState(null);
+  const [maxScroll, setMaxScroll] = useState(9);
 
   useEffect(() => {
     const query = `
@@ -99,15 +101,22 @@ function NewsPosts({searchQuery, isFiltering}) {
 
   if (filteredPosts.length > 0) {
     return (
-      <Container fixed style={{marginTop: '40px'}}>
-        <Grid container spacing={3} rowSpacing={2}>
-          {filteredPosts.map(newsPost => (
-            <Grid item xs={12} sm={12} md={6} lg={4} key={newsPost.sys.id}>
-              <NewsPostCard newsPost={newsPost}></NewsPostCard>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+      <>
+        <Container fixed style={{marginTop: '40px'}}>
+          <Grid container spacing={3} rowSpacing={2}>
+            {filteredPosts.slice(0, maxScroll).map(newsPost => (
+              <Grid item xs={12} sm={12} md={6} lg={4} key={newsPost.sys.id}>
+                <NewsPostCard newsPost={newsPost}></NewsPostCard>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+        <Waypoint
+          onEnter = {() => {
+            setMaxScroll(maxScroll + 6);
+          }}
+        ><div style={{height: '50px'}}/></Waypoint>
+      </>
     );
   } else {
     return (
