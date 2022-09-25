47a89bb825e689a72e847286460e72a65a5af0ab
Guido Gloor <haslo@haslo.ch>
2022-09-25 14:02:47 +0200

    Changed the SoundCloud track again - have to do this in the CMS instead

--- src/SoundCloudIFrame.js
+++ src/SoundCloudIFrame.js
@@ -3,7 +3,7 @@ import {Container} from "@mui/material";
 function SoundCloudIFrame() {
   const iframeHtml = '<iframe title="Featured Track" ' +
     'width="100%" height="200" scrolling="no" frameborder="no" allow="autoplay" ' +
-    'src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1343244064&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">' +
+    'src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1350414166&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">' +
     '</iframe>'
   return (
     <Container fixed style={{marginTop: '100px'}}>
