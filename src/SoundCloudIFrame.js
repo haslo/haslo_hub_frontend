56e471834c3183e1b916b156eb238514340e9849
Guido Gloor <haslo@haslo.ch>
2024-01-06 13:14:28 +0100

    New track featured

--- src/SoundCloudIFrame.js
+++ src/SoundCloudIFrame.js
@@ -4,7 +4,7 @@ function SoundCloudIFrame() {
   const iframeHtml = '<iframe title="Featured Track" ' +
     'width="100%" height="200" scrolling="no" frameborder="no" allow="autoplay" ' +
     'src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' +
-    '1601101920' +
+    '1706379294' +
     '&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">' +
     '</iframe>'
   return (
