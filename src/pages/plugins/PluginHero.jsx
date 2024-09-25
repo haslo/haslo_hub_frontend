166365944a911dc5a96c13267e8a91c9a8e7a4f2
Guido Gloor <haslo@haslo.ch>
2024-09-25 17:09:29 +0200

    Hero unit for placeholder

---
+++ src/pages/plugins/PluginHero.jsx
@@ -0,0 +1,25 @@
+import {
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
+
+import * as React from "react";
+
+export function PluginHero({hero_title, hero_content}) {
+  return (
+    <>
+      <Grid item xs={12} sm={12} md={12} lg={12}>
+        <Card sx={{paddingX: 1, marginBottom: '25px'}}>
+          <CardContent>
+            <Typography variant='h4' sx={{textAlign: 'center', marginBottom: '20px'}}>
+              {hero_title}
+            </Typography>
+            {hero_content}
+          </CardContent>
+        </Card>
+      </Grid>
+    </>
+  );
+}
--- src/pages/plugins/Plugins.jsx
+++ src/pages/plugins/Plugins.jsx
@@ -6,11 +6,28 @@ import {PluginCard} from "./PluginCard";
 import {Destruktor} from "./Destruktor";
 import {Haaschen} from "./Haaschen";
 import {Ploink} from "./Ploink";
+import {PluginHero} from "./PluginHero";
 
 export function Plugins() {
   return (
     <>
       <Container fixed style={{marginTop: '40px', marginBottom: '100px'}}>
+        <Grid container spacing={1} rowSpacing={1}>
+          <PluginHero
+            hero_title={"Voltage Modular / Legacy"}
+            hero_content={
+              <>
+                I've created these plugins a while ago. Years, in fact.
+                Most of them were finished in December 2020, and released in early 2021.
+                <br/><br/>
+                Consider this page to be a bit of a placeholder. The modules still work of course! But I have many
+                fancier things planned, including a revamped and improved and extended DAW plugin version of DeSTRuKTor.
+                <br/><br/>
+                Watch this space 😅
+              </>
+            }
+          />
+        </Grid>
         <Grid container spacing={3} rowSpacing={2}>
           <PluginCard
             avatar_name={"Voltage Modular Module"}
