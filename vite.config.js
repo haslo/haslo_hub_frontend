91edae2dd01b7527047553866082d09d13af331f
Guido Gloor <haslo@haslo.ch>
2024-10-08 12:49:23 +0200

    Ignore consent for main script

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// Custom plugin to add data-cookieconsent="ignore" to the main script
+const addCookieConsentIgnore = () => {
+  return {
+    name: 'add-cookie-consent-ignore',
+    transformIndexHtml(html) {
+      return html.replace(
+        /<script type="module" crossorigin/,
+        '<script type="module" crossorigin data-cookieconsent="ignore"'
+      )
+    }
+  }
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    addCookieConsentIgnore()
+  ],
   server: {
     open: true,
   },
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+})
